1d37d0c64af3d12c2ca40c06324e0e334165faa0
megumi.kimura <megu.megu.megu68@gmail.com>
2020-12-09 10:20:37 +0000

    edit3

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
- 
-  
+  before_action :correct_user, only: [:destroy, :show, :update, :edit]
  
   def index
     #@tasks = Task.all
@@ -52,25 +50,19 @@ class TasksController < ApplicationController
   end
   
   private
-  
-  def set_task
-    #@task = Task.find(params[:id])
-    @task = current_user.tasks.find(params[:id])
-   
-  end
 
   # Strong Parameter
   def task_params
     params.require(:task).permit(:content,:status)
   end
-  
-  def correct_user
+end
+
+ def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
       redirect_to root_url
     end
-  end
-end
+ end
 
 
 
